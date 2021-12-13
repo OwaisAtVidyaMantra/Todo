2f5e8be624dcaa193049d6732016c914a561df98
Mohd Owais <mohd.owais@vidyamantra.com>
2021-12-13 15:55:34 +0530

    conflic sortd

--- src/components/UIElements/CardsLayout.vue
+++ src/components/UIElements/CardsLayout.vue
@@ -1,16 +1,10 @@
 <template>
-<<<<<<< HEAD
-  <v-card class="mx-auto" max-width="374">
-    <img height="300" :src="imageSrc" />
-    <p> </p>
-=======
   <div>
     <v-card class="mx-auto pa-8" max-width="374">
     <v-img class="mx-auto"
       height="250"
       :src="getImgUrl(imageCard)"
     ></v-img>
->>>>>>> f387bad8c70192e4424d8ea833cffe4c9249506d
 
     <v-card-title>Cafe Badilico</v-card-title>
 
@@ -29,10 +23,6 @@
 </template>
 <script>
 export default {
-<<<<<<< HEAD
-  props: ["imageSrc"],
-};
-=======
   props:{
     imageCard:String
   },
@@ -42,7 +32,6 @@ export default {
 }
 }
 }
->>>>>>> f387bad8c70192e4424d8ea833cffe4c9249506d
 </script>
 <style scoped>
 
--- src/components/UIElements/MainContent.vue
+++ src/components/UIElements/MainContent.vue
@@ -1,30 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    <v-container class="section1">
-      <v-row no-gutters class="pa-20" style="height: 980px">
-        <v-col lg="4" align-self="center">
-          <h1>Why Exam Path Finder?</h1>
-          {{ cardImg }}
-          
-          <img height="300" :src="cardImg" alt="img" />
-
-          <br />
-          <cards-layout></cards-layout>
-        </v-col>
-        <v-col lg="4" align-self="end">
-          <cards-layout :imageSrc="cardImg"></cards-layout><br />
-          <cards-layout imageSrc="b1"></cards-layout>
-        </v-col>
-        <v-col lg="4" align-self="start">
-          <cards-layout
-            imageSrc="https://exampathfinder.com/img/connect_with_community2.f1a32736.png"
-          ></cards-layout>
-          <br />
-          <cards-layout
-            imageSrc="https://exampathfinder.com/img/notification_feature.b0a9c247.png"
-          ></cards-layout>
-=======
   <div class="section1"> 
     <v-container style="height: 100%">
       <v-row no-gutters class="pa-20" >
@@ -40,7 +14,6 @@
         <v-col lg="4" align-self="start">
           <cards-layout imageCard="b4.png"></cards-layout> <br />
           <cards-layout imageCard="b5.png"></cards-layout>
->>>>>>> f387bad8c70192e4424d8ea833cffe4c9249506d
         </v-col>
       </v-row>
     </v-container>
@@ -54,25 +27,9 @@
 </template>
 <script>
 import CardsLayout from "./CardsLayout.vue";
-<<<<<<< HEAD
-const x = require('../../assets/b1.png')
-export default {
-  components: { "cards-layout": CardsLayout },
-  data() {
-    return {
-      cardImg: x,
-    };
-  },
-  computed: {
-    //   img() {
-    //     //   return "../src/assets/b1.png";
-    //   }
-  }
-=======
 import Faq from './Faq.vue';
 export default {
   components: { CardsLayout, Faq },
->>>>>>> f387bad8c70192e4424d8ea833cffe4c9249506d
 };
 </script>
 <style scoped>
